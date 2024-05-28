7052ece7f1a53c91a07abeb1b3144d011ce3f53a
DanCmo <dankurso845@gmail.com>
2024-05-27 21:00:01 -0500

    Implementación del primer microservicio, para hacer CRUD a buscadores y postulantes

---
+++ Personas/src/main/java/co/edu/udistrital/Personas/controller/BuscadorController.java
@@ -0,0 +1,51 @@
+package co.edu.udistrital.Personas.controller;
+
+import co.edu.udistrital.Personas.entity.Buscador;
+import co.edu.udistrital.Personas.service.BuscadorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+@RequestMapping("/buscador")
+public class BuscadorController {
+    @Autowired
+    private BuscadorService buscadorService;
+
+    @GetMapping("/{cedula}")
+    public ResponseEntity<Buscador> consultarBuscador(@PathVariable String cedula) {
+        Buscador buscador = buscadorService.consultarBuscador(cedula);
+        if (buscador != null) {
+            return ResponseEntity.ok(buscador);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<String> registrarBuscador(@RequestBody Buscador buscador) {
+        Buscador verificar = buscadorService.consultarBuscador(buscador.getCedula());
+        if (verificar == null) {
+            buscadorService.crearBuscador(buscador);
+            return ResponseEntity.ok("Buscador creado exitosamente");
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("El buscador ya existe");
+        }
+
+    }
+
+    @PutMapping("/{cedula}")
+    public ResponseEntity<String> actualizarBuscador(@PathVariable String cedula, @RequestBody Buscador buscadorActualizado) {
+        buscadorService.actualizarBuscador(cedula, buscadorActualizado);
+        return ResponseEntity.ok("Buscador actualizado exitosamente");
+    }
+
+    @DeleteMapping("/{cedula}")
+    public ResponseEntity<String> eliminarBuscador(@PathVariable String cedula) {
+        buscadorService.eliminarBuscador(cedula);
+        return ResponseEntity.ok("Se eliminó al buscador con éxito");
+    }
+
+}

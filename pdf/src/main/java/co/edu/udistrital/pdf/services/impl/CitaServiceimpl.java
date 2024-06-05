5d09eab764221232423169b8e6f75bcd043b743b
Alex012505 <giovannialexander25@gmail.com>
2024-06-05 17:07:17 -0500

    d

--- pdf/src/main/java/co/edu/udistrital/pdf/services/impl/CitaServiceimpl.java
+++ pdf/src/main/java/co/edu/udistrital/pdf/services/impl/CitaServiceimpl.java
@@ -22,32 +22,48 @@ public class CitaServiceimpl implements iCitaService {
 
     @Override
     public ByteArrayInputStream generatePdf() {
-        Cita[] citaList = restTemplate.getForObject("http://localhost:8082/citas/listar",Cita[].class);
+        Cita[] citaList = restTemplate.getForObject("http://localhost:8082/citas/listar ",Cita[].class);
+
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        try {
-            PdfWriter writer = new PdfWriter(out);
-            PdfDocument pdf = new PdfDocument(writer);
-            Document document = new Document(pdf);
-            document.add(new Paragraph("Listado de Citas"));
-            Table table = new Table(UnitValue.createPercentArray(4)).useAllAvailableWidth();
-            table.addHeaderCell("Nombre Buscador");
-            table.addHeaderCell("Puntuacion Buscador");
-            table.addHeaderCell("Nombre Postulante");
-            table.addHeaderCell("Puntuación postulante");
-            for (Cita cita : citaList) {
-                table.addCell(cita.getBuscador());
-                table.addCell(cita.getCalificacionBuscador());
-                table.addCell(cita.getPostulante());
-                table.addCell(cita.getCalificacionPostulante());
+            try {
+                PdfWriter writer = new PdfWriter(out);
+                PdfDocument pdf = new PdfDocument(writer);
+                Document document = new Document(pdf);
+                document.add(new Paragraph("Listado de Citas Amistad"));
+                Table table = new Table(UnitValue.createPercentArray(4)).useAllAvailableWidth();
+                document.add(new Paragraph("Listado de Citas Mas que amistad"));
+                Table table2=new Table(UnitValue.createPercentArray(4)).useAllAvailableWidth();
+                table.addHeaderCell("Nombre Buscador");
+                table.addHeaderCell("Puntuacion Buscador");
+                table.addHeaderCell("Nombre Postulante");
+                table.addHeaderCell("Puntuación postulante");
+                table2.addHeaderCell("Nombre Buscador");
+                table2.addHeaderCell("Puntuacion Buscador");
+                table2.addHeaderCell("Nombre Postulante");
+                table2.addHeaderCell("Puntuación postulante");
+                for (Cita cita : citaList) {
+                    if (cita.getCalificacion() == "amistad"){
+                        table.addCell(cita.getBuscador());
+                        table.addCell(cita.getCalificacionBuscador());
+                        table.addCell(cita.getPostulante());
+                        table.addCell(cita.getCalificacionPostulante());
+                    }else{
+                        table.addCell(cita.getBuscador());
+                        table.addCell(cita.getCalificacionBuscador());
+                        table.addCell(cita.getPostulante());
+                        table.addCell(cita.getCalificacionPostulante());
+
+                }}
+
+
+                document.add(table);
+                document.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
             }
-            document.add(table);
-            document.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-
+            return new ByteArrayInputStream(out.toByteArray());
         }
-        return new ByteArrayInputStream(out.toByteArray());
-    }
 }
